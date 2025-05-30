699aa895c4d5f57a4e6abea38598450039469ca7
votre_nom <laposta.enzo@gmail.com>
2025-05-30 10:07:39 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+function App() {
+
+  return (
+    <div className="bg-slate-700 text-slate-100 pt-20 min-h-screen">
+      <div className="max-w-xl mx-auto border border-slate-500 rounded p-10">
+        <h1 className="text-4xl text-center mb-8">Pomodoro App</h1>
+
+        <div className="flex justify-center mb-8">
+          <div className="mr-10">
+            <p className="text-center mb-1">Sessions</p>
+            <div className="flex">
+              <p className="mx-4 text-xl">25</p>
+            </div>
+          </div>
+
+          <div >
+            <p className="text-center mb-1">Pauses</p>
+            <div className="flex">
+              <p className="mx-4 text-xl">55</p>
+            </div>
+          </div>
+        </div>
+
+        <p className="text-center mb-2 text-xl font-semibold">Work</p>
+
+        <p className="text-center flex justify-center mb-1">
+          <span className="text-4xl p-4 rounded bg-slate-300 text-slate-900">25:00</span>
+        </p>
+        <p className="mb-10 text-center">Passed cycle(s) : 0</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
